9a06385c68951481f2bd6fe7202037b9a64b6c7e
albertx0 <pinheiro.gabriel2020@gmail.com>
2024-10-06 22:19:49 -0300

    Subrotina para mapear teclas

--- main.asm
+++ main.asm
@@ -4,12 +4,12 @@ SJMP MAIN
 ORG 0030H
 
 MAIN: 
-MOV P1, #00; inicializa o led
-CALL INICIAR; chamar a rotina para iniciar
+	MOV P1, #00; inicializa o led
+	CALL INICIAR; chamar a rotina para iniciar
 
-;---LOOP principal para checar senha
-MOV R0, #04 ; senha de 4 digitos
-MOV DPTR, #SENHA_CORRETA ; aponta para endereço da senha corretz
+	;---LOOP principal para checar senha
+	MOV R0, #04 ; senha de 4 digitos
+	MOV DPTR, #SENHA_CORRETA ; aponta para endereço da senha corretz
 
 
 INICIAR:
@@ -21,13 +21,27 @@ LER_SENHA:
 ;nao sei fazer eu acho
 
 ALARME_ATIVADO:
-SET P1.0 ;acende o led no p1.0 para alarme ativo para senha correta
-RET
+	SET P1.0 ;acende o led no p1.0 para alarme ativo para senha correta
+	RET
 
 
 SENHA_INCORRETA:
-CLR P1.0 ; apaga o led no p1.9 para indicar senha incorreta
-RET
+	CLR P1.0 ; apaga o led no p1.9 para indicar senha incorreta
+	RET
 
 SENHA_CORRETA:
     DB 01H, 02H, 03H, 04H  ; Define a senha como 1234
+
+MAPEAR_TECLAS: ; Subrotina para mapear
+	MOV 40H, #'#' ; e facilitar a identificacao 
+	MOV 41H, #'0' ; de qual tecla foi pressionada
+	MOV 42H, #'*'
+	MOV 43H, #'9'
+	MOV 44H, #'8'
+	MOV 45H, #'7'
+	MOV 46H, #'6'
+	MOV 47H, #'5'
+	MOV 48H, #'4'
+	MOV 49H, #'3'
+	MOV 4AH, #'2'
+	MOV 4AH, #'1'

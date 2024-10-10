21b1c96a38c01b8e91d5b22ed87517e7881137a2
albertx0 <126724209+albertx0@users.noreply.github.com>
2024-10-09 22:04:22 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -5,33 +5,36 @@ ORG 0030H
 
 MAIN: 
 	MOV P1, #00; inicializa o led
-	CALL INICIAR; chamar a rotina para iniciar
+	ACALL INICIAR; chamar a rotina para iniciar
 
 	;---LOOP principal para checar senha
 	MOV R0, #04 ; senha de 4 digitos
-	MOV DPTR, #senhaIncorreta ; aponta para endereço da senha corretz
+	MOV DPTR, #senhaIncorreta ; aponta para endereço da senha correta
 
 
 iniciar:
     ; Rotina de inicialização
     MOV P1, #00H    ; Apaga o LED
-    acall fim
+   	ljmp fim
 
 alarmeAtivado:
 	SETB P1.0 ;acende o led no p1.0 para alarme ativo para senha correta
-	acall fim
+	ljmp fim
 
 senhaIncorreta:
 	CLR P1.0 ; apaga o led no p1.9 para indicar senha incorreta
-	 acall fim
+	lmjp fim
 
 senhasCorreta:
     DB 01H, 02H, 03H, 04H  ; Define a senha como 1234
 
-mapearTeclas: ; Subrotina para mapear
-	MOV 40H, #'#' ; e facilitar a identificacao 
-	MOV 41H, #'0' ; de qual tecla foi pressionada
-	MOV 42H, #'*'
+; Subrotina para mapear
+; e facilitar a identificacao
+; de qual tecla foi pressionada
+mapearTeclas: 
+	MOV 40H, #'#' ; Botao para confirmar senha digitada 
+	MOV 41H, #'0' 
+	MOV 42H, #'*' ; Botao para apagar a senha digitada
 	MOV 43H, #'9'
 	MOV 44H, #'8'
 	MOV 45H, #'7'
@@ -42,18 +45,29 @@ mapearTeclas: ; Subrotina para mapear
 	MOV 4AH, #'2'
 	MOV 4AH, #'1'
 
+registrarSenha:
+	MOV b, #04h ; Contador de quantos digitos recebeu
+	MOV R1, #20h ; Endereco onde vai registrar a senha
+	ACALL receberSenha
+
+receberSenha:
+	ACALL leituraTeclado
+	JNB F0, receberSenha ; Caso nao houve leitura de algum digito
+	djnz b, fim
+	mov a, 	
+
 pegarChave:
 	SETB F0
-	 acall fim
+	LJMP fim
 
 lerColuna:
 	JNB P0.4, pegarChave
 	INC R0
-	JNB  P0.5, pegarChave
+	JNB P0.5, pegarChave
 	INC R0
 	JNB P0.6, pegarChave
 	INC R0
-	acall fim
+	ljpm fim
 	
 lerTeclado:
 	MOV R0, #00
@@ -78,7 +92,11 @@ lerTeclado:
 	CALL lerColuna
 	JB F0, fim
 
-alarmeAtivido:
-	
+verificarAlarme:
+
+delay:
+	MOV R7, #50
+	DJNZ R7, $
+	fim
 fim:
 	ret

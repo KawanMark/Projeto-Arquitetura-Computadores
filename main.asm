cabd86a5acbf1ea4dfdc27c0bacac8cd347a2a09
Kawan Mark Silva <128873271+KawanMark@users.noreply.github.com>
2024-11-02 23:53:51 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,24 +1,20 @@
+RS      equ     P1.3   
+EN      equ     P1.2    
+senha   EQU 20h   ; Endereço para armazenar a senha
+input   EQU 30h   ; Endereço para armazenar a tentativa de entrada
+msg1:
+    DB "Crie uma senha"
+    DB 0
+
+msg2:
+    DB "Informe a senha"
+    DB 0
+
 ORG 0000H
-SJMP MAIN
+LJMP START
 
-senha EQU 20h   ; Endereço para armazenar a senha
-input EQU 30h   ; Endereço para armazenar a tentativa de entrada
-
-MAIN:
-    MOV P1, #00      ; Inicializa o LED apagado
-    ACALL iniciar    ; Chama a rotina de iniciar (registra a primeira senha)
-    ACALL registrarInput  ; Captura a segunda entrada do usuário para verificação
-    ACALL verificarInput  ; Verifica se a senha está correta
-    SJMP $           ; Loop infinito para parar o programa
-
-iniciar:
-    ACALL mapearTeclas
-    ACALL registrarSenha  ; Registra a senha inicial
-    RET
-
-mapearTeclas:
-    ; Mapeia as teclas para o teclado
-    MOV 40H, #'#'
+START:
+    MOV 40H, #'#' 
     MOV 41H, #'0'
     MOV 42H, #'*'
     MOV 43H, #'9'
@@ -29,8 +25,98 @@ mapearTeclas:
     MOV 48H, #'4'
     MOV 49H, #'3'
     MOV 4AH, #'2'
-    MOV 4BH, #'1'
+    MOV 4BH, #'1'	
+
+MAIN:
+    ACALL lcd_init
+
+    MOV A, #00h
+    CALL posicionaCursor
+    MOV DPTR, #msg1
+    CALL escreveString
+    CALL delay
+    
+    ACALL iniciar
+
+    MOV A, #00h
+    CALL posicionaCursor
+    MOV DPTR, #msg2
+    CALL escreveString
+    CALL registrarInput
+	
+    ACALL verificarInput
+    SJMP $           ; Loop infinito para parar o programa
+
+iniciar:
+    ACALL registrarSenha  ; Registra a senha inicial
     RET
+
+; ------------------- Subrotinas do LCD -------------------;
+lcd_init:
+    CLR RS		
+    CLR P1.7
+    CLR P1.6	
+    SETB P1.5
+    CLR P1.4
+    SETB EN
+    CLR EN	
+    CALL delay
+    SETB EN		
+    CLR EN	
+    SETB P1.7
+    SETB EN
+    CLR EN
+    CALL delay
+    CLR P1.7
+    CLR P1.6
+    CLR P1.5	
+    CLR P1.4	
+    SETB EN	
+    CLR EN	
+    SETB P1.6
+    SETB P1.5
+    SETB EN
+    CLR EN	
+    CALL delay
+    CLR P1.7
+    CLR P1.6
+    CLR P1.5
+    CLR P1.4
+    SETB EN	
+    CLR EN	
+    CALL delay
+    RET
+
+; ------------------- Subrotinas ativar o LED P1.1 -------------------;
+piscarP1_1:
+    MOV R2, #5          ; Número de vezes que o LED irá piscar
+piscar_loop_1:
+    SETB P1.1           ; Liga o LED em P1.1
+    ACALL delay
+    CLR P1.1           ; Desliga o LED em P1.1
+    ACALL delay
+    DJNZ R2, piscar_loop_1 ; Repete o piscar 5 vezes
+    RET
+
+; Limpa o display
+clearDisplay:
+    CLR RS	
+    CLR P1.7		
+    CLR P1.6	
+    CLR P1.5		
+    CLR P1.4		
+    SETB EN		
+    CLR EN		
+    CLR P1.7	
+    CLR P1.6	
+    CLR P1.5		
+    SETB P1.4		
+    SETB EN		
+    CLR EN		
+    CALL delay	
+    RET
+
+; ------------------- Subrotinas para receber a senha ------------------;
 
 registrarSenha:
     MOV B, #04h      ; Contador de 4 dígitos
@@ -45,7 +131,6 @@ registrarInput:
     RET
 
 receberSenha:
-    ; Rotina para capturar 4 dígitos de senha ou entrada
     MOV B, #04h          ; Reinicia o contador de dígitos
 receber_loop:
     ACALL lerTeclado     ; Chama a rotina para ler o teclado
@@ -56,6 +141,7 @@ receber_loop:
     DJNZ B, receber_loop ; Decrementa o contador e repete até 4 dígitos
     RET
 
+; ------------------- Subrotinas ativar o alarme ------------------;
 piscarLeds:
     MOV R2, #5          ; Número de vezes que os LEDs irão piscar
 piscar_loop:
@@ -66,36 +152,105 @@ piscar_loop:
     DJNZ R2, piscar_loop ; Repete o piscar 5 vezes
     RET
 
+; ------------------- Subrotinas para envio de msgs para LCD ------------------;
+
+enviarCaractere:
+    SETB RS  	
+    MOV C, ACC.7	
+    MOV P1.7, C		
+    MOV C, ACC.6	
+    MOV P1.6, C		
+    MOV C, ACC.5	
+    MOV P1.5, C		
+    MOV C, ACC.4
+    MOV P1.4, C			
+    SETB EN		
+    CLR EN			
+    MOV C, ACC.3		
+    MOV P1.7, C			
+    MOV C, ACC.2		
+    MOV P1.6, C			
+    MOV C, ACC.1		
+    MOV P1.5, C			
+    MOV C, ACC.0	
+    MOV P1.4, C			
+    SETB EN
+    CLR EN	
+    CALL delay
+    CALL delay
+    RET
+
+escreveString:
+    MOV R2, #00h
+rot:
+    MOV A, R2
+    MOVC A, @A+DPTR
+    ACALL enviarCaractere
+    INC R2
+    JNZ rot
+    RET
+
+; ------------------- Subrotinas para posicionamentos do cursor ------------------;
+
+posicionaCursor:
+    CLR RS	
+    SETB P1.7
+    MOV C, ACC.6
+    MOV P1.6, C
+    MOV C, ACC.5
+    MOV P1.5, C
+    MOV C, ACC.4
+    MOV P1.4, C
+    SETB EN		
+    CLR EN	
+    MOV C, ACC.3
+    MOV P1.7, C
+    MOV C, ACC.2
+    MOV P1.6, C
+    MOV C, ACC.1
+    MOV P1.5, C
+    MOV C, ACC.0
+    MOV P1.4, C	
+    SETB EN
+    CLR EN	
+    CALL delay
+    CALL delay
+    RET
+
+; ------------------- Subrotinas do teclado -------------------;
+
 lerTeclado:
-    MOV R0, #00         ; Inicializa R0 com 0 para capturar o valor da tecla
-    MOV P0, #0FFH       ; Configura as portas P0 para alto
-    CLR P0.0            ; Ativa a primeira coluna
-    CALL lerColuna      ; Chama a leitura das colunas
-    JB F0, fim          ; Se uma tecla foi lida, sai da rotina
-
-    SETB P0.0           ; Desativa a primeira coluna
-    CLR P0.1            ; Ativa a segunda coluna
+    MOV R0, #0          ; Limpa R0 - começa pela tecla 0
+    MOV P0, #0FFh
+    ; Varre linha 0
+    CLR P0.0
     CALL lerColuna
-    JB F0, fim
-
-    SETB P0.1           ; Desativa a segunda coluna
-    CLR P0.2            ; Ativa a terceira coluna
+    JB F0, finish       ; Se tecla encontrada, sai
+    ; Varre linha 1
+    SETB P0.0
+    CLR P0.1
     CALL lerColuna
-    JB F0, fim
-
-    SETB P0.2           ; Desativa a terceira coluna
-    CLR P0.3            ; Ativa a quarta coluna (se aplicável)
+    JB F0, finish
+    ; Varre linha 2
+    SETB P0.1
+    CLR P0.2
     CALL lerColuna
-    JB F0, fim
+    JB F0, finish
+    ; Varre linha 3
+    SETB P0.2
+    CLR P0.3
+    CALL lerColuna
+    JB F0, finish
 
+finish:
     RET
 
 lerColuna:
-    JNB P0.4, pegarChave ; Verifica se a tecla da coluna 1 foi pressionada
+    JNB P0.4, pegarChave
     INC R0
-    JNB P0.5, pegarChave ; Verifica se a tecla da coluna 2 foi pressionada
+    JNB P0.5, pegarChave
     INC R0
-    JNB P0.6, pegarChave ; Verifica se a tecla da coluna 3 foi pressionada
+    JNB P0.6, pegarChave
     INC R0
     RET
 
@@ -104,8 +259,9 @@ pegarChave:
     MOV A, R0            ; Carrega o valor da tecla
     RET
 
+; ------------------- Subrotinas para verificar a senha ------------------;
+
 verificarInput:
-    ; Comparar a senha com a entrada
     MOV R0, #senha   ; Ponteiro para a senha armazenada
     MOV R1, #input   ; Ponteiro para a entrada do usuário
     MOV R2, #04h     ; Número de dígitos para comparar
@@ -116,21 +272,17 @@ senha_igual:
     CJNE A, B, erro  ; Se forem diferentes, vai para a rotina de erro
     INC R0           ; Avança para o próximo dígito da senha
     INC R1           ; Avança para o próximo dígito da entrada
-    DJNZ R2, senha_igual  ; Continua verificando os próximos dígitos
+    DJNZ R2, senha_igual
+    ACALL piscarP1_1  ; Chama a sub-rotina para piscar P1.1 se a senha estiver correta
     RET              ; Se todas as comparações forem iguais, a senha está correta
 
 erro:
-    ; Se a senha estiver incorreta, aciona o alarme (pisca os LEDs)
-    ACALL piscarLeds
+    ACALL piscarLeds  ; Chama a sub-rotina para piscar o LED em P1.0 se a senha estiver errada
     RET
 
 delay:
-    MOV R7, #150     ; Tempo de atraso ajustável
-    ACALL timer
-    RET
-
-timer:
-    DJNZ R7, timer
+    MOV R7, #50
+    DJNZ R7, $  
     RET
 
 fim:
